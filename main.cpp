c3b1bd32437cb9b9b82f359f9493c9d8d4350481
jacopomazzo <114420544+jacopomazzo@users.noreply.github.com>
2022-12-13 10:04:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+float massimo(float mat[],int n){
+    float a = mat[0];
+    for(int i=1;i<n;i++){
+        if(mat[i] > a){
+            a = mat[i];
+        }
+    }
+    return a;
+}
+
 int main() {
     int n = 0;
     cin >> n;
