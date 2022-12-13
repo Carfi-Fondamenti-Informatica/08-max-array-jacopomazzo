bbbc1d0a92b3bef58c54d5f40c991034bfb5ecc0
jacopomazzo <114420544+jacopomazzo@users.noreply.github.com>
2022-12-13 10:02:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
 
+float massimo(float mat[],int n){
+    float a = mat[0];
+    for(int i=1;i<n;i++){
+        if(mat[i] > a){
+            a = mat[i];
+        }
+    }
+    return a;
+}
